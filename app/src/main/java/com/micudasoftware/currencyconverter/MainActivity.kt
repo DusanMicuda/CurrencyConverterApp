e592331a224a419be450b39bad891795b3afb683
Dušan Mičuda <micudasoftware@gmail.com>
2023-09-16 17:43:51 +0200

    💄 Add CurrencyRatesScreen to MainActivity

--- app/src/main/java/com/micudasoftware/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/micudasoftware/currencyconverter/MainActivity.kt
@@ -6,41 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.micudasoftware.currencyconverter.ui.screen.CurrencyRatesScreen
 import com.micudasoftware.currencyconverter.ui.theme.CurrencyConverterTheme
+import com.micudasoftware.currencyconverter.ui.viewmodel.CurrencyRatesViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModel<CurrencyRatesViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewState = viewModel.viewState.collectAsState()
             CurrencyConverterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CurrencyRatesScreen(viewState = viewState)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CurrencyConverterTheme {
-        Greeting("Android")
     }
 }
