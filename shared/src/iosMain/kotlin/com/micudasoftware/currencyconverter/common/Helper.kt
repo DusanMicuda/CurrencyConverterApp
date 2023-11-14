42790c43c2dff59d6b598331b55c1b7c55c19c1f
Dušan Mičuda <micudasoftware@gmail.com>
2023-11-14 16:33:38 +0100

    🐛 Fix iOS theme

--- shared/src/iosMain/kotlin/com/micudasoftware/currencyconverter/common/Helper.kt
+++ shared/src/iosMain/kotlin/com/micudasoftware/currencyconverter/common/Helper.kt
@@ -7,15 +7,18 @@ import androidx.compose.ui.window.ComposeUIViewController
 import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
 import cafe.adriel.voyager.navigator.tab.TabNavigator
 import com.micudasoftware.currencyconverter.di.appModule
+import com.micudasoftware.currencyconverter.presentation.common.theme.CurrencyConverterTheme
 import com.micudasoftware.currencyconverter.presentation.feature.currencyconverter.CurrencyConverterScreen
 import org.koin.core.context.startKoin
 
 @OptIn(ExperimentalMaterialApi::class)
 fun MainViewController() = ComposeUIViewController {
-    BottomSheetNavigator(
-        sheetShape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
-    ) {
-        TabNavigator(CurrencyConverterScreen())
+    CurrencyConverterTheme {
+        BottomSheetNavigator(
+            sheetShape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
+        ) {
+            TabNavigator(CurrencyConverterScreen())
+        }
     }
 }
 
