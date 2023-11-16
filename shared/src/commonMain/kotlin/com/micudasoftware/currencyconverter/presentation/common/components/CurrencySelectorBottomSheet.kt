3bbe72fb01f1b8fc0407a758bc00dbb492696afd
Dušan Mičuda <micudasoftware@gmail.com>
2023-11-16 13:56:27 +0100

    🐛 Fix CurrencySelectorBottomSheet in dark mode

--- shared/src/commonMain/kotlin/com/micudasoftware/currencyconverter/presentation/common/components/CurrencySelectorBottomSheet.kt
+++ shared/src/commonMain/kotlin/com/micudasoftware/currencyconverter/presentation/common/components/CurrencySelectorBottomSheet.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,7 +31,7 @@ import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
 
 /**
- * Bottom sheet select to select currency.
+ * Bottom sheet selector to select currency.
  *
  * @param currencies List of selectable currencies.
  * @param onSelectCurrency Lambda function that is called on currency select.
@@ -39,68 +41,64 @@ class CurrencySelectorBottomSheet(
     private val onSelectCurrency: (Currency) -> Unit
 ): Screen {
 
-    @Composable
-    override fun Content() {
-        Screen(currencies = currencies, onSelectCurrency = onSelectCurrency)
-    }
-
     @OptIn(ExperimentalFoundationApi::class)
     @Composable
-    fun Screen(
-        currencies: List<Currency>,
-        onSelectCurrency: (Currency) -> Unit
-    ) {
+    override fun Content() {
         val bottomSheetNavigator = LocalBottomSheetNavigator.current
 
-        Column(
-            modifier = Modifier.fillMaxWidth()
+        Surface(
+            color = MaterialTheme.colorScheme.surfaceVariant
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+            Column(
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text(
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .weight(1f)
-                        .padding(start = 48.dp),
-                    text = stringResource(SharedRes.strings.select_base_currency),
-                    textAlign = TextAlign.Center,
-                )
-                IconButton(
-                    modifier = Modifier
-                        .padding(end = 24.dp)
-                        .size(24.dp),
-                    onClick = { bottomSheetNavigator.hide() }
+                        .padding(vertical = 24.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Icon(
-                        painterResource(SharedRes.images.ic_close),
-                        contentDescription = null
-                    )
-                }
-            }
-            LazyColumn {
-                items(currencies) { currency ->
-                    Row(
+                    Text(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .clickable {
-                                bottomSheetNavigator.hide()
-                                onSelectCurrency(currency)
-                            }
-                            .padding(horizontal = 20.dp, vertical = 16.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
+                            .weight(1f)
+                            .padding(start = 48.dp),
+                        text = stringResource(SharedRes.strings.select_base_currency),
+                        textAlign = TextAlign.Center,
+                    )
+                    IconButton(
+                        modifier = Modifier
+                            .padding(end = 24.dp)
+                            .size(24.dp),
+                        onClick = { bottomSheetNavigator.hide() }
                     ) {
-                        Text(
-                            modifier = Modifier.basicMarquee(),
-                            text = "${currency.name?.getString()} (${currency.id})",
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1
+                        Icon(
+                            painterResource(SharedRes.images.ic_close),
+                            contentDescription = null
                         )
                     }
-                    Divider(modifier = Modifier.fillMaxWidth())
+                }
+                LazyColumn {
+                    items(currencies) { currency ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    bottomSheetNavigator.hide()
+                                    onSelectCurrency(currency)
+                                }
+                                .padding(horizontal = 20.dp, vertical = 16.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                modifier = Modifier.basicMarquee(),
+                                text = "${currency.name?.getString()} (${currency.id})",
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                        }
+                        Divider(modifier = Modifier.fillMaxWidth())
+                    }
                 }
             }
         }
